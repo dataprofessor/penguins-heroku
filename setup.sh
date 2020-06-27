006d45000edc5a81472c94d7d5cd754b14c7790c
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2020-06-27 10:14:27 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,9 @@
 mkdir -p ~/.streamlit/
-echo “\
-[general]\n\
-email = \”your-email@domain.com\”\n\
-“ > ~/.streamlit/credentials.toml
-echo “\
+
+echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
-“ > ~/.streamlit/config.toml
+enableCORS = false\n\
+\n\
+" > ~/.streamlit/config.toml
